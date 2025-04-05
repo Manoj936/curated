ba7b8536ee5bda203b7de9c4749dc8dd5ec17c42
Manoj Mohapatra <manoj.mohapatra986@gmail.com>
2025-04-05 23:37:25 +0530

    adding versioning

---
+++ app/api/v1/imagekit-auth/route.ts
@@ -0,0 +1,24 @@
+import ImageKit from "imagekit";
+import { NextRequest, NextResponse } from "next/server";
+
+const configuration = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY as string,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
+  urlEndpoint: process.env.IMAGEKIT_PUBLIC_URL_ENDPOINT as string,
+});
+
+export async function GET(request: NextRequest) {
+  try {
+    const authenticationParameters =
+      configuration.getAuthenticationParameters();
+    return NextResponse.json(authenticationParameters);
+  } catch (err) {
+   
+    return NextResponse.json(
+      { error: "Authentication failed" },
+      {
+        status: 500,
+      }
+    );
+  }
+}
